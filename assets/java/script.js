31e38c87475b7a20c70c2a9b6b12dc57a811c991
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2023-12-04 12:03:00 +0330

    added function to check time from work and if at work how much time left of work there is

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,57 +1,98 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDay = dayjs().format("DD/MM/YYYY");
-var currentHour = dayjs().format("hh");
-var amPm = dayjs().format("a");
 
 $(function () {
+  //sets current date and time updates every second
+  var currentDay = dayjs().format("MMM DD YYYY");
+  var amPm = dayjs().format("a");
+  var currentHour = dayjs().format("hh");
   $("#currentDay").text("Today is " + currentDay);
-  $("#currentTime").text("It is currently " + currentHour +' '+ amPm);
 
+  function timeUpdate() {
+    var currentTime = dayjs().format("hh:mm:ss");
+
+    $("#currentTime").text("It is currently " + currentTime + " " + amPm);
+  }
+
+  setInterval(timeUpdate, 1000);
+  // sets class past, present, or future depending on relation with current hour 
   function blockColor() {
-    $('.time-block').each(function(){
+    $(".time-block").each(function () {
       var block = parseInt(this.id);
-      $(this).toggleClass('.past', block < currentHour);
-      $(this).toggleClass('.present',block === currentHour);
-      $(this).toggleClass('.future',block > currentHour);
+      $(this).toggleClass(".past", block < currentHour);
+      $(this).toggleClass(".present", block === currentHour);
+      $(this).toggleClass(".future", block > currentHour);
     });
-  };
-
+  }
+  // updates block color depending on set class 
   function updateBlockColor() {
-    $('.time-block').each(function(){
+    $(".time-block").each(function () {
       var block = parseInt(this.id);
-      if (block == currentHour){
-        $(this).removeClass('past future').addClass("present");
-      }else if (block < currentHour){
+      if (block == currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else if (block < currentHour) {
         $(this).removeClass("present future").addClass("past");
-      }else{
+      } else {
         $(this).removeClass("past present").addClass("future");
       }
-    }); 
-  };
-  
+    });
+  }
+  // stores text and corresponding time block id into local storage when save button is pressed 
+  function textStorage() {
+    $(".saveBtn").on("click", function () {
+      var key = $(this).parent().attr("id");
+      var value = $(this).siblings(".description").val();
+      localStorage.setItem(key, value);
+    });
+  }
+  //displays saved text on corresponding time block from local storage if none is saved time blocks will be blank
+  $(".time-block").each(function () {
+    var key = $(this).attr("id");
+    var value = localStorage.getItem(key);
+    $(this).children(".description").val(value);
+  });
+  // clears local storage and refreshes page when clear btn is pressed 
+  function clearLocalStorage() {
+    localStorage.clear();
+    location.reload();
+  }
+  $(".clear").on("click", clearLocalStorage);
 
-  
-  
+  //Checks too see how many hours till work or
+  //if you are at work how may hours left at work or
+  //if you are done with work how many hours left of the day.
+  //Displays proper message
+  function isItTime() {
+    currentHour = dayjs().get("hour");
+    console.log(currentHour);
+    var morning = 9;
+    var work = 17;
+    var evening = 23;
 
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // T ODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-blockColor();
-updateBlockColor();
+    var workStartsIn = morning - currentHour;
+    var atWork = work - currentHour;
+    var doneWithWork = evening - currentHour;
+
+    console.log(workStartsIn);
+
+    if (currentHour < 9) {
+      $("#timeTillWork").text("Work Starts in " + workStartsIn + " hours");
+    } else if (currentHour > 9 && currentHour < 17) {
+      $("#timeTillWork").text(
+        "You are currently at work. You have " +
+          atWork +
+          " hours left! You're doing great."
+      );
+    } else {
+      $("#timeTillWork").text(
+        "You are done with work. You have " + doneWithWork + " hours to relax."
+      );
+    }
+  }
+  //runs functions
+  isItTime();
+  textStorage();
+  blockColor();
+  updateBlockColor();
 });
